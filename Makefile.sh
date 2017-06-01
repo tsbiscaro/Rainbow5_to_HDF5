c6ec72c43bae88a43ce7f844342536dcc48cf2e3
Thiago Biscaro <tsbiscaro@gmail.com>
2017-06-01 14:51:31 -0300

    Makefile -> alteracao de libhdf pra hdf_serial

--- Makefile.sh
+++ Makefile.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#rm *.o
-#gcc -I/usr/include/libxml2 rainbow5_to_hdf5.c write_hdf5.c le_cabecalho_xml.c  -c
-#g++ le_dados_blob.cpp -I/usr/include/qt4 -I/usr/include/libxml2 -c
-g++ -I/usr/include/libxml2  -I/usr/include/qt4  rainbow5_to_hdf5.c  write_hdf5.c le_cabecalho_xml.c le_dados_blob.cpp -lhdf5  -lxml2 -lQtCore -g -w -o rainbow5_to_hdf5
+g++ -I/usr/include/libxml2  -I/usr/include/qt4 -I/usr/include/hdf5/serial rainbow5_to_hdf5.c  write_hdf5.c le_cabecalho_xml.c le_dados_blob.cpp -lhdf5_serial  -lxml2 -lQtCore -g -w -o rainbow5_to_hdf5
 ulimit -s unlimited

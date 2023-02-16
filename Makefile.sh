6a94422f8de04c5ed5de77c69d966bb3e96b584f
Thiago S. Biscaro <tsbiscaro@gmail.com>
2023-02-16 09:26:38 -0300

    Alteracao do Makefile.sh para libQt versao 5

--- Makefile.sh
+++ Makefile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ -I/usr/include/libxml2  -I/usr/include/qt4 -I/usr/include/hdf5/serial rainbow5_to_hdf5.c  write_hdf5.c le_cabecalho_xml.c le_dados_blob.cpp -lhdf5_serial  -lxml2 -lQtCore -g -w -o rainbow5_to_hdf5
+g++ -I/usr/include/libxml2  -I/usr/include/x86_64-linux-gnu/qt5 -I/usr/include/hdf5/serial rainbow5_to_hdf5.c  write_hdf5.c le_cabecalho_xml.c le_dados_blob.cpp -lhdf5_serial  -lxml2 -lQt5Core -g -w -o rainbow5_to_hdf5 -fPIC
 ulimit -s unlimited
